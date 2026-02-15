50e6d10a71b6c34cecd3669360a5b208ea8d6fa7
Deepak Kumar <deepakkumar@Deepaks-MacBook-Air.local>
2026-02-15 12:57:06 +0530

    2 separate landing pages for admins and students

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,37 @@
 import { Redirect, Slot, useSegments } from "expo-router";
 import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, View } from "react-native";
-import { auth } from "./firebaseSetup/firebaseSetup";
+import { auth, db } from "./firebaseSetup/firebaseSetup";
 
 export default function RootLayout() {
   const [user, setUser] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const segments = useSegments();
 
+  // if user is of role admin, redirect to admin dashboard
+  const getRoleAsync = async (uid: string) => {
+    try {
+      const studentsDoc = await getDoc(doc(db, "students", uid));
+      const adminsDoc = await getDoc(doc(db, "admins", uid));
+      console.log({ studentsDoc, adminsDoc });
+      // return doc.data()?.role || "user";
+      return adminsDoc.exists() ? (
+        <Redirect href="/admin" />
+      ) : (
+        <Redirect href="/pages/dashboard" />
+      ); // Placeholder for testing
+    } catch (e) {
+      console.error("Error fetching user role:", e);
+      return "student"; // Default to student on error
+    }
+  };
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (u) => {
       setUser(u);
+      u && u.uid && getRoleAsync(u.uid);
       setLoading(false);
     });
 
@@ -25,16 +45,18 @@ export default function RootLayout() {
       </View>
     );
   }
-
-  const inAuthGroup = segments[0] === "(auth)";
+  console.log("Segments:", segments);
+  const isInsideAuthRouteSegment = segments[0] === "(auth)";
 
   // If not logged in and NOT already on login → redirect to login
-  if (!user && !inAuthGroup) {
+  if (!user && !isInsideAuthRouteSegment) {
+    console.log("User not logged in, redirecting to login page.");
     return <Redirect href="/(auth)/login" />;
   }
 
   // If logged in and currently in auth → redirect to pages
-  if (user && inAuthGroup) {
+  if (user && isInsideAuthRouteSegment) {
+    console.log("User is logged in, redirecting to dashboard.");
     return <Redirect href="/pages/dashboard" />;
   }
 
---
+++ app/admin/_layout.tsx
@@ -0,0 +1,15 @@
+import { Slot } from "expo-router";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const AdminLayout = () => {
+  return (
+    <View>
+      <Slot />
+    </View>
+  );
+};
+
+export default AdminLayout;
+
+const styles = StyleSheet.create({});
