d3bb19eb3631dfc4b05d0041cc0bbcca17b1d0b9
Deepak Kumar <deepakkumar@Deepaks-MacBook-Air.local>
2026-03-15 00:17:48 +0530

    changing configs

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -58,7 +58,7 @@ export default function Login() {
     >
       <View style={styles.card}>
         <Text style={styles.title}>Welcome Back!</Text>
-        <Text style={styles.subtitle}>Login to your school-app account</Text>
+        <Text style={styles.subtitle}>Login to your KPS account</Text>
         <TextInput
           style={styles.input}
           placeholder="Email"
