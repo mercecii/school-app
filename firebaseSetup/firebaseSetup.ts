c82a58d37939f871bbe80c6b32884755fd1c1009
mercecii <deepakkumar@Deepaks-MacBook-Air.local>
2026-03-19 12:22:44 +0530

    comments clean-up

--- firebaseSetup/firebaseSetup.ts
+++ firebaseSetup/firebaseSetup.ts
@@ -13,26 +13,6 @@ const firebaseConfig = {
   appId: "1:198371863429:web:bd6cb1d9ced76fb8f472ec",
 };
 
-// const app = initializeApp(firebaseConfig);
-
-// let auth;
-
-// if (Platform.OS === "web") {
-//   // Web uses default browser persistence automatically
-//   auth = getAuth(app);
-// } else {
-//   // React Native needs explicit AsyncStorage persistence
-//   // ✅ Proper React Native auth with persistence
-//   const { getReactNativePersistence } = require("firebase/auth/react-native");
-//   const persistence = getReactNativePersistence(AsyncStorage);
-//   console.log("persistence = ", persistence);
-
-//   auth = initializeAuth(app, {
-//     persistence,
-//   });
-//   // Auth emulator removed for production
-// }
-
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 // Firestore
